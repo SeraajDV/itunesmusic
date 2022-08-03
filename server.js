c818ce1df8931016b6311b46add546bf93e54a9e
fiona <fibsy79@yahoo.com>
2022-08-03 11:37:28 +0200

    added changes

--- server.js
+++ server.js
@@ -24,7 +24,15 @@ app.get('/api/:searchTerm/:mediaType', (req, res) => {
 })
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,
+            'frontend', 'build', 'index.html'));
+    });
+}
 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
 
